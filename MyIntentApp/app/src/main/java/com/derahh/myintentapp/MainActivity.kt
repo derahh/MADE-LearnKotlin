d6d4d6140be2414ad49454e48ba1732945b6b26e
Dedi Rahmansah <derah666@gmail.com>
2019-10-30 20:25:46 +0700

    done code

--- MyIntentApp/app/src/main/java/com/derahh/myintentapp/MainActivity.kt
+++ MyIntentApp/app/src/main/java/com/derahh/myintentapp/MainActivity.kt
@@ -1,12 +1,73 @@
 package com.derahh.myintentapp
 
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var tvResult: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn_move_activity.setOnClickListener(this)
+        btn_move_activity_data.setOnClickListener(this)
+        btn_move_activity_object.setOnClickListener(this)
+        btn_dial_number.setOnClickListener(this)
+        btn_move_for_result.setOnClickListener(this)
+
+        tvResult = findViewById(R.id.tv_result)
+    }
+
+    override fun onClick(v: View) {
+        when(v.id){
+            R.id.btn_move_activity -> {
+                val moveIntent = Intent(this@MainActivity, MoveActivity::class.java)
+                startActivity(moveIntent)
+            }
+
+            R.id.btn_move_activity_data -> {
+                val moveWithDataIntent = Intent(this@MainActivity, MoveWithDataActivity::class.java)
+                moveWithDataIntent.putExtra(MoveWithDataActivity.EXTRA_NAME, "Dedi Rahmansah")
+                moveWithDataIntent.putExtra(MoveWithDataActivity.EXTRA_AGE, 20)
+                startActivity(moveWithDataIntent)
+            }
+
+            R.id.btn_move_activity_object -> {
+                val person = Person("Dedi Rahmansah", 20, "dedir2@gmail.com", "Bandung")
+                val moveWithObjectIntent = Intent(this@MainActivity, MoveWithObjectActivity::class.java)
+                moveWithObjectIntent.putExtra(MoveWithObjectActivity.EXTRA_PERSON, person)
+                startActivity(moveWithObjectIntent)
+            }
+
+            R.id.btn_dial_number -> {
+                val phoneNumber = "081299439698"
+                startActivity(Intent(Intent.ACTION_DIAL, Uri.parse("tel:$phoneNumber")))
+            }
+
+            R.id.btn_move_for_result -> {
+                val moveForResultIntent = Intent(this@MainActivity, MoveForResultActivity::class.java)
+                startActivityForResult(moveForResultIntent, MoveForResultActivity.RESULT_CODE)
+            }
+        }
+    }
+
+    @SuppressLint("SetTextI18n")
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == MoveForResultActivity.RESULT_CODE){
+            if (resultCode == MoveForResultActivity.RESULT_CODE){
+                val selectedValue = data?.getIntExtra(MoveForResultActivity.EXTRA_SELECTED_VALUE, 0)
+                tvResult.text = "Hasil : $selectedValue"
+            }
+        }
     }
 }
