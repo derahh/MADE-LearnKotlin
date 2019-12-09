e9e27d355b1b1ba346de822445f449fc761ec188
Dedi Rahmansah <derah666@gmail.com>
2019-12-09 12:33:17 +0700

    Update MainActivity.kt

--- MyTestingApp/app/src/main/java/com/derahh/mytestingapp/MainActivity.kt
+++ MyTestingApp/app/src/main/java/com/derahh/mytestingapp/MainActivity.kt
@@ -2,11 +2,26 @@ package com.derahh.mytestingapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var btnSetValue:Button
+    private lateinit var tvText:TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tvText = findViewById(R.id.tv_text)
+        btnSetValue.setOnClickListener(this)
+    }
+
+    override fun onClick(p0: View) {
+        if (p0.id == R.id.btn_set_value) {
+            tvText.text = "19"
+        }
     }
 }
