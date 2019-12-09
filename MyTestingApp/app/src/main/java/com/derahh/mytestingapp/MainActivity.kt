bdc5eb2afb00501a6ebf9381117af8763577ae9b
Dedi Rahmansah <derah666@gmail.com>
2019-12-09 12:44:32 +0700

    Update MainActivity.kt

--- MyTestingApp/app/src/main/java/com/derahh/mytestingapp/MainActivity.kt
+++ MyTestingApp/app/src/main/java/com/derahh/mytestingapp/MainActivity.kt
@@ -5,23 +5,34 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
+import java.lang.StringBuilder
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
 
-    private lateinit var btnSetValue:Button
     private lateinit var tvText:TextView
+
+    private var names = ArrayList<String>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         tvText = findViewById(R.id.tv_text)
-        btnSetValue.setOnClickListener(this)
+        btn_set_value.setOnClickListener(this)
+
+        names.add("Dedi Rahmansah")
+        names.add("Fadli")
+        names.add("Haris")
     }
 
     override fun onClick(p0: View) {
         if (p0.id == R.id.btn_set_value) {
-            tvText.text = "19"
+            val name = StringBuilder()
+            for (i in 0..3) {
+                name.append(names[i]).append("\n")
+            }
+            tvText.text = name.toString()
         }
     }
 }
