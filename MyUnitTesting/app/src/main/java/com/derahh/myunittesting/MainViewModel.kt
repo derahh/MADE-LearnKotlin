b046b00e57ba82e8facb5bd875f2e03fa6e0289a
Dedi Rahmansah <derah666@gmail.com>
2019-12-15 21:31:58 +0700

    Update MainViewModel.kt

--- MyUnitTesting/app/src/main/java/com/derahh/myunittesting/MainViewModel.kt
+++ MyUnitTesting/app/src/main/java/com/derahh/myunittesting/MainViewModel.kt
@@ -1,2 +1,13 @@
 package com.derahh.myunittesting
 
+class MainViewModel(private val cuboidModel: CuboidModel) {
+    fun getCircumference(): Double = cuboidModel.getCircumference()
+
+    fun getSurfaceArea(): Double = cuboidModel.getSurfaceArea()
+
+    fun getVolume(): Double = cuboidModel.getVolume()
+
+    fun save(l: Double, w: Double, h: Double) {
+        cuboidModel.save(w, l, h)
+    }
+}
