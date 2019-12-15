0fcfd4ca4d067e809903fa17c89556652540b6bc
Dedi Rahmansah <derah666@gmail.com>
2019-12-15 22:44:28 +0700

    Update MainViewModelTest.kt

--- MyUnitTesting/app/src/test/java/com/derahh/myunittesting/MainViewModelTest.kt
+++ MyUnitTesting/app/src/test/java/com/derahh/myunittesting/MainViewModelTest.kt
@@ -4,7 +4,7 @@ import org.junit.Test
 
 import org.junit.Assert.*
 import org.junit.Before
-import org.mockito.Mockito.mock
+import org.mockito.Mockito.*
 
 class MainViewModelTest {
 
@@ -51,5 +51,29 @@ class MainViewModelTest {
         cuboidModel = CuboidModel()
         mainViewModel = MainViewModel(cuboidModel)
         mainViewModel.save(dummyLength, dummyWidth, dummyHeight)
+    }
+
+    @Test
+    fun testMockVolume() {
+        `when`(mainViewModel.getVolume()).thenReturn(dummyVolume)
+        val volume = mainViewModel.getVolume()
+        verify(cuboidModel).getVolume()
+        assertEquals(dummyVolume, volume, 0.0001)
+    }
+
+    @Test
+    fun testMockCircumference() {
+        `when`(mainViewModel.getCircumference()).thenReturn(dummyCircumference)
+        val volume = mainViewModel.getCircumference()
+        verify(cuboidModel).getCircumference()
+        assertEquals(dummyCircumference, volume, 0.0001)
+    }
+
+    @Test
+    fun testMockSurfaceArea() {
+        `when`(mainViewModel.getSurfaceArea()).thenReturn(dummySurfaceArea)
+        val volume = mainViewModel.getSurfaceArea()
+        verify(cuboidModel).getSurfaceArea()
+        assertEquals(dummySurfaceArea, volume, 0.0001)
     }
 }
