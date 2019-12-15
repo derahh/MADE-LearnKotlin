8078638bda6b7b0fdfa0786f9dc9fc27992ac5d1
Dedi Rahmansah <derah666@gmail.com>
2019-12-15 22:23:47 +0700

    Create MainViewModelTest.kt

---
+++ MyUnitTesting/app/src/test/java/com/derahh/myunittesting/MainViewModelTest.kt
@@ -0,0 +1,24 @@
+package com.derahh.myunittesting
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+class MainViewModelTest {
+
+    @Test
+    fun getCircumference() {
+    }
+
+    @Test
+    fun getSurfaceArea() {
+    }
+
+    @Test
+    fun getVolume() {
+    }
+
+    @Test
+    fun save() {
+    }
+}
