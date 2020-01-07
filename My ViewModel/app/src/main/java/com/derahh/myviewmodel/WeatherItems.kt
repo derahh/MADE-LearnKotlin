e8559a192e42e39f7d31e991894c34a82471d8f2
Dedi Rahmansah <derah666@gmail.com>
2020-01-07 14:00:39 +0700

    Update WeatherItems.kt

--- My ViewModel/app/src/main/java/com/derahh/myviewmodel/WeatherItems.kt
+++ My ViewModel/app/src/main/java/com/derahh/myviewmodel/WeatherItems.kt
@@ -1,2 +1,9 @@
 package com.derahh.myviewmodel
 
+class WeatherItems {
+    var id: Int = 0
+    var name: String? = null
+    var currentWeather: String? = null
+    var description: String? = null
+    var temperature: String? = null
+}
