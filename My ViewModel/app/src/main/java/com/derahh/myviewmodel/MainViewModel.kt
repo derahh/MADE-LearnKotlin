7f3c366a523c823dcbd665c31f6b2d58cb0d6d26
Dedi Rahmansah <derah666@gmail.com>
2020-01-07 14:20:51 +0700

    Update MainViewModel.kt

--- My ViewModel/app/src/main/java/com/derahh/myviewmodel/MainViewModel.kt
+++ My ViewModel/app/src/main/java/com/derahh/myviewmodel/MainViewModel.kt
@@ -1,2 +1,20 @@
 package com.derahh.myviewmodel
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    companion object {
+        private const val API_KEY = "f37c56e150010286197fb57628c9c35d"
+    }
+    val listWeathers = MutableLiveData<ArrayList<WeatherItems>>()
+
+    internal fun setWeather(cities: String) {
+        //request API
+    }
+
+    internal fun getWeather(): LiveData<ArrayList<WeatherItems>> {
+        return listWeathers
+    }
+}
